2c494d75b420f28fefb247947a6fe0a0eb9be618
Steve Fischer <sfischer@upenn.edu>
2013-04-23 14:02:54 +0000

    add domains freq file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWorkflow/trunk@55059 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeDownloadFiles.pm
+++ Main/lib/perl/WorkflowSteps/MakeDownloadFiles.pm
@@ -27,19 +27,31 @@ sub run {
     $self->runCmd($test, "rm $seqsDownloadFileName.gz");
     $self->runCmd($test, "rm $deflinesDownloadFileName.gz");
     $self->runCmd($test, "rm $groupsDownloadFileName.gz");
+    $self->runCmd($test, "rm $domainsDownloadFileName.gz");
     $self->runCmd($test, "rm -r $pairsDownloadDirName.tar.gz");
   } else {
+
+    # fasta
     my $sql = $self->getSql(1);
     $self->runCmd($test, "gusExtractSequences --outputFile $seqsDownloadFileName --idSQL \"$sql\"");
     $self->runCmd($test, "gzip $seqsDownloadFileName");
 
+    # deflines
     $sql = $self->getSql(0);
     $self->runCmd($test, "gusExtractSequences --outputFile $deflinesDownloadFileName --idSQL \"$sql\" --noSequence");
     $self->runCmd($test, "gzip $deflinesDownloadFileName");
 
+    # domain frequencies
+    my $extDbRlsId = $self->getExtDbRlsId($test, "PFAM|" . $self->getExtDbVersion($test, "PFAM"));
+    $sql = $self->getDomainsSql($extDbRlsId);
+    $self->runCmd($test, "makeFileWithSql --outFile $domainsDownloadFileName --sql \"$sql\"");
+    $self->runCmd($test, "gzip $domainsDownloadFileName");
+
+    # groups
     $self->runCmd($test, "cp $workflowDataDir/$groupsDir/groups.txt $groupsDownloadFileName");
     $self->runCmd($test, "gzip $groupsDownloadFileName");
 
+    # pairs
     $self->runCmd($test, "cp -r $workflowDataDir/$groupsDir/pairs $websiteFilesDir/$downloadSiteDir/$pairsDownloadDirName");
     $self->runCmd($test, "tar -czf $websiteFilesDir/$downloadSiteDir/$pairsDownloadDirName.tar.gz $websiteFilesDir/$downloadSiteDir/$pairsDownloadDirName");
   }
@@ -57,4 +69,25 @@ select ot.three_letter_abbrev || '|' || x.source_id || ' | ' ||
                    where ot.taxon_id = x.taxon_id and x.aa_sequence_id = oga.aa_sequence_id(+)
                    and oga.ortholog_group_id = og.ortholog_group_id(+)
 ";
+}
+
+sub getDomainsSql {
+  my ($self, $extDbRlsId) = @_;
+
+return "
+SELECT name as Orthomcl_Group, primary_identifier as Pfam_Name, round(count(aa_sequence_id)/number_of_members,4) as Frequency from (
+SELECT distinct og.name, db.primary_identifier, ogs.aa_sequence_id, og.number_of_members
+FROM apidb.OrthologGroupAaSequence ogs,
+apidb.OrthologGroup og,
+dots.DomainFeature df,
+dots.DbRefAaFeature dbaf,
+sres.DbRef db
+WHERE og.ortholog_group_id != 0 
+AND ogs.ortholog_group_id = og.ortholog_group_id
+AND df.aa_sequence_id = ogs.aa_sequence_id
+AND dbaf.aa_feature_id = df.aa_feature_id
+AND db.db_ref_id = dbaf.db_ref_id
+and db.external_database_release_id = $extDbRlsId)
+group by name, number_of_members, primary_identifier
+ORDER BY name, frequency desc";
 }
