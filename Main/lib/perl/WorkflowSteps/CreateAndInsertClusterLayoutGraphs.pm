ad79372ae44cea836b4f3e9177902f1925babece
Steve Fischer <sfischer@upenn.edu>
2014-10-14 19:04:44 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWorkflow/trunk@65005 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CreateAndInsertClusterLayoutGraphs.pm
@@ -0,0 +1,23 @@
+package OrthoMCLWorkflow::Main::WorkflowSteps::CreateAndInsertClusterGraphs;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $threads = $self->getParamValue('numberOfThreadsToUse');
+    my $maxGroupSize = $self->getParamValue('maxGroupSize');
+
+    if ($undo) {
+      $self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -u");
+    } else{
+      $self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -m $maxGroupSize -t $threads");
+      }
+    }
+}
+
+
