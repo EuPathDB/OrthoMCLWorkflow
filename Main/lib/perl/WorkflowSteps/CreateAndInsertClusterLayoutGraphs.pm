dd00b10f4ce357dd3c199aa5786d5f0fa912724e
Deborah Pinney <pinney@upenn.edu>
2014-11-07 15:09:08 +0000

    remove extra curly bracket that prevented compiling
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWorkflow/trunk@65251 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CreateAndInsertClusterLayoutGraphs.pm
+++ Main/lib/perl/WorkflowSteps/CreateAndInsertClusterLayoutGraphs.pm
@@ -8,15 +8,14 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 
 sub run {
     my ($self, $test, $undo) = @_;
-
+    
     my $threads = $self->getParamValue('numberOfThreadsToUse');
     my $maxGroupSize = $self->getParamValue('maxGroupSize');
 
     if ($undo) {
-      $self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -u");
+	$self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -u");
     } else{
-      $self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -m $maxGroupSize -t $threads");
-      }
+	$self->runCmd($test, "orthomclClusterLayout -d ApiDB.GroupLayout -m $maxGroupSize -t $threads");
     }
 }
 
