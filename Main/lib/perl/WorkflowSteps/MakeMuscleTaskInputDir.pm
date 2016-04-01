f0efc90bb143b95d2e9a259fc3655062c1c8f696
Deborah Pinney <pinney@upenn.edu>
2016-04-01 14:27:52 +0000

    add muscleBinDir to task.prop
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWorkflow/trunk@73737 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeMuscleTaskInputDir.pm
+++ Main/lib/perl/WorkflowSteps/MakeMuscleTaskInputDir.pm
@@ -15,8 +15,10 @@ sub getTaskPropFileContents {
     my ($self, $clusterWorkflowDataDir) = @_;
     # get parameters
     my $fastaFilesDir = $self->getParamValue('fastaFilesDir');
+    my $binDir = $self->getConfig('muscleBinDir');
     return 
 "inputFileDir=$clusterWorkflowDataDir/$fastaFilesDir
+ muscleBinDir=$binDir
 ";
 }
 
