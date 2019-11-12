f845c15d1c29101942b0162476b315adbaeee9ac
Mark Hickman <markhick@sas.upenn.edu>
2019-11-12 18:54:28 -0500

    add step in orthomcl peripheral workflow to combine residual fasta files

---
+++ Main/lib/perl/WorkflowSteps/CombineResidualFiles.pm
@@ -0,0 +1,30 @@
+package OrthoMCLWorkflow::Main::WorkflowSteps::CombineResidualFiles;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $residualFastaFilesDir = $self->getParamValue('residualFastaFilesDir');
+  my $combinedResidualsFile = $self->getParamValue('combinedResidualsFile');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  if ($undo) {
+    $self->runCmd(0, "rm $workflowDataDir/$combinedResidualsFile") if -e "$workflowDataDir/$combinedResidualsFile";
+  } else {
+    $self->testInputFile('combinedResidualsFile', "$workflowDataDir/$combinedResidualsFile");
+
+    my $cmd = "orthomclCombineResidualFiles $workflowDataDir/$residualFastaFilesDir $workflowDataDir/$combinedResidualsFile";
+    $self->runCmd($test, $cmd);
+    if ($test) {
+      $self->runCmd(0, "touch $workflowDataDir/$combinedResidualsFile");
+    }
+
+  }
+}
+
+
