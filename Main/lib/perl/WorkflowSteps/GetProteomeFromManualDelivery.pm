1f4610c2872a26778a955f3b82431dc0f4804c1a
Mark Hickman <markhick@sas.upenn.edu>
2020-12-22 11:37:50 -0500

    workflow steps perl module for peripheral manual delivery

---
+++ Main/lib/perl/WorkflowSteps/GetProteomeFromManualDelivery.pm
@@ -0,0 +1,31 @@
+package OrthoMCLWorkflow::Main::WorkflowSteps::GetProteomeFromManualDelivery;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $manualDeliveryFile = $self->getParamValue('manualDeliveryFile');
+  my $outputDir = $self->getParamValue('outputDir');
+  my $outputFile = $self->getParamValue('outputFile');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  $outputDir = "$workflowDataDir/$outputDir";
+  $outputFile = "$outputDir/$outputFile";
+  my $zipped = $outputFile.".gz";
+
+  if ($undo) {
+    $self->runCmd(0, "if [-e $zipped]; then rm $zipped; fi");
+    $self->runCmd(0, "if [-e $outputFile]; then rm $outputFile; fi");
+  } else {
+    $self->runCmd($test,"cp $manualDeliveryFile $outputDir");
+    $self->runCmd($test,"if [-e $zipped]; then gunzip -d $zipped; fi";
+  }
+}
+
+1;
+
